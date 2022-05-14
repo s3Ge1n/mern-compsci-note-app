3eb29eb8bf335109326107b9a07680074c5ef738
s3Geln <55173869+Domdadomb@users.noreply.github.com>
2022-05-14 15:06:49 +0800

    prepare deploy

---
+++ frontend/src/features/notes/noteService.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const API_URL = '/api/notes/';
+
+
+//create new note
+const createNote = async (noteData, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.post(API_URL, noteData, config);
+
+    return response.data;
+}
+
+//get user notes
+const getNotes = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.get(API_URL, config);
+
+    return response.data;
+}
+
+// delete user note
+const deleteNote = async (noteId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+    }
+
+    const response = await axios.delete(API_URL + noteId, config);
+
+    return response.data;
+}
+
+const noteService = {
+    createNote,
+    getNotes,
+    deleteNote,
+}
+
+export default noteService;
---
+++ frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,112 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import noteService from './noteService';
+
+const initialState = {
+    notes: [],
+    isError: false,
+    isLoading: false,
+    isSuccess: false,
+    message: ''
+}
+
+// create new note
+export const createNote = createAsyncThunk('notes/create', async (noteData, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await noteService.createNote(noteData, token)
+    } catch (error) {
+        const message =
+        (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+        error.message ||
+        error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+});
+
+// get user notes
+export const getNotes = createAsyncThunk('notes/getAll', async (_, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await noteService.getNotes(token)
+    } catch (error) {
+        const message =
+        (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+        error.message ||
+        error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+});
+
+// delete user note
+export const deleteNote = createAsyncThunk('notes/delete', async (id, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await noteService.deleteNote(id, token)
+    } catch (error) {
+        const message =
+        (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+        error.message ||
+        error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+});
+
+
+export const noteSlice = createSlice({
+    name: 'notes',
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createNote.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(createNote.fulfilled, (state, action) => {
+                state.notes.push(action.payload)
+                state.isLoading = false
+                state.isSuccess = true
+            })
+            .addCase(createNote.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+            .addCase(getNotes.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(getNotes.fulfilled, (state, action) => {
+                state.notes = action.payload
+                state.isLoading = false
+                state.isSuccess = true
+            })
+            .addCase(getNotes.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+            .addCase(deleteNote.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(deleteNote.fulfilled, (state, action) => {
+                state.notes = state.notes.filter((note) => note._id !== action.payload.id)
+                state.isLoading = false
+                state.isSuccess = true
+            })
+            .addCase(deleteNote.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+    },
+})
+
+export const {reset} = noteSlice.actions
+export default noteSlice.reducer
